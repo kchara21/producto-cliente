ff558b6fd9eb9b47a1bcb0eabcf26d0a06d280df
Kaleb Chara Toala <kalebdavidchara@hotmail.com>
2025-11-27 08:57:51 -0500

    Use environment files for auth configuration

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -4,13 +4,14 @@ import {
     Configuration,
     RedirectRequest,
 } from '@azure/msal-browser';
+import { environment } from '../environment';
 
 const origin = window.location.origin;
 
 export const msalConfig: Configuration = {
     auth: {
-        clientId: '4335a4d4-0ff4-4964-8677-0b1c29dbf529',
-        authority: 'https://login.microsoftonline.com/0b506217-0d47-4361-9327-8f74192bb5c5',
+        clientId: environment.clientId,
+        authority: environment.authority,
         redirectUri: `${origin}/`,
     },
     cache: {
---
+++ src/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  clientId: '4335a4d4-0ff4-4964-8677-0b1c29dbf529',
+  authority: 'https://login.microsoftonline.com/0b506217-0d47-4361-9327-8f74192bb5c5'
+};
